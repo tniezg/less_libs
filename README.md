22b8e6d211bf5f82ee3acd09f3e577c45452d18e
Tomasz Niezgoda <tomasz.niezgoda@senfino.com>
2014-03-05 13:14:33 +0100

    Added examples, removed excessive libs.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #LESS complimentary styles
 
 It uses [Bower](http://bower.io/) to download dependant libraries.
+Examples require LESS >= 1.4.0.
--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,21 +1,5 @@
 module.exports = function(grunt) {
-	var sourcePath = '../app',
-		buildPath = '../build',
-		cssPath = sourcePath,
-		destinationCssPath = buildPath + '/mixins.css';
-
 	grunt.initConfig({
-		less: {
-			development: {
-				src: [
-					cssPath + '/mixins.less'
-				],
-				dest: destinationCssPath,
-				options: {
-					compress: true
-				}
-			}
-		},
 		bump: {
 			options: {
 				files: ["../bower.json"],
@@ -23,28 +7,8 @@ module.exports = function(grunt) {
 				push: false
 				// commit: false
 			}
-		},
-		watch: {
-			development: {
-				files: [
-					sourcePath + '/**'
-				],
-				tasks: [
-					'less:development'
-				],
-				options: {
-					nospawn: true,
-					livereload: true
-				}
-			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-bump');
-
-	grunt.registerTask('dev', [
-		'watch:development'
-	]);
 };
